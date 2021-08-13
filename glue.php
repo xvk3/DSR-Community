d38f2e53fe89b5ca7bcc3b466ee06b7682c6a560
W <w@xvk3.net>
2021-08-13 12:12:50 +0100

    frontend overhaul!

--- glue.php
+++ glue.php
@@ -3,7 +3,75 @@
   
   <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"/>
-    <title>Community Glue</title>
+    <title>DSR-Community</title>
+    <style>
+
+    * {
+      box-sizing: border-box;
+      margin: 0 auto;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    .container {
+      display: flex;
+      flex-wrap: wrap;
+      width: 950px;
+    }
+
+    .container > .reg,
+    .container > .dsr {
+      margin: 5px;
+      padding: 10px;
+      background: #242322;
+      border-radius: 5px;
+      flex: 0 46%;
+      box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+    }
+
+    .container > div h1 {
+      margin: 5px;
+      color: #eba434;
+      font-size: 1em;
+      text-shadow: 4px 2px rgba(0, 0, 0, 0.75);
+    }
+
+    .container > div img {
+      float: left;
+      margin-right: 12px;
+      border-radius: 15px;
+    }
+
+    .dsr img {
+      /*border: 3px solid rgba(94, 214, 96, 0.5); */
+      box-shadow: rgba(94, 214, 96, 0.5) 0px 0px 0px 3px,
+      rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
+      rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+    }
+
+    .reg p,
+    .dsr p {
+      color: #fff;
+      margin: 5px;
+    }
+
+    .reg a,
+    .dsr a {
+      font-size: 1.25em;
+      top: 0;
+      color: #999;
+      text-shadow: 4px 2px rgba(0, 0, 0, 0.25);
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+
+    .dsr a{
+      color: rgb(94, 214, 96, 0.5);
+    }
+
+    </style>
   </head>
   <body>
   <?php
@@ -20,34 +88,77 @@
   $obj = json_decode($xml, true);
 
   $friends[] = array();
+  $master[]  = array();
 
   foreach($obj['friendslist']['friends'] as $friend)  {
     array_push($friends, $friend['steamid']);
   }
 
   // use array_push to add the SteamIDs of players that aren't on my friendslist
-  //array_push($friends, "SteamID of somebody");
+  array_push($friends, "561198016305721");
+  array_push($friends, "561198029258748");
+  array_push($friends, "561198043194261");
+  array_push($friends, "561198056522377");
+  array_push($friends, "561198057963176");
+  array_push($friends, "561198059025420");
+  array_push($friends, "561198103682933");
+  array_push($friends, "561198234856584");
+  array_push($friends, "561198262293303");
+  array_push($friends, "561198281879760");
+  array_push($friends, "561198326431359");
+  array_push($friends, "561198329986125");
+  array_push($friends, "561198995963731");
+  array_push($friends, "561198996550548");
+  array_push($friends, "561199007281262");
+  array_push($friends, "561199085632748");
 
-  $ids = "";
+  // Ensure there are no duplicates in the array
+  $friends = array_unique($friends);
 
-  if(count($friends) > 100) {
-    echo "too many people, dm Mich\n";
-    die();
-    // ugh gonna have to split it up into a series of 100 steamids
-  } else {
+  if(count($friends) <= 100) {
+
     $ids = implode(",", $friends);
-    //echo $ids;
+
+    $url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0001/?key=" . $KEY . "&steamids=" . $ids;
+    $xml = file_get_contents($url);
+    $obj = json_decode($xml, true);
+    $sub = $obj['response']['players']['player'];
+    // Prepare $master with player objects
+    for($i = 0; $i < count($sub); $i+=1)  {
+      array_push($master, $obj['response']['players']['player'][$i]);
+    }
+  } else {
+    $start = 0;
+    while($start != count($friends) - 1)  {                // When $start is the size of the array (minus 1) it means we are done
+      $sub = array_slice($friends, $start, 100, false);    // Take 100 elements from $friends[$start]
+      $start += count($sub);                               // Increment $start by number of elements taken (will be 100 until the last one)
+      $ids = implode(",", $sub);
+ 
+      $url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0001/?key=" . $KEY . "&steamids=" . $ids;
+      $xml = file_get_contents($url);
+      $obj = json_decode($xml, true);
+      $sub = $obj['response']['players']['player'];
+      // Prepare $master with player objects
+      for($i = 0; $i < count($sub); $i+=1)  {
+        array_push($master, $obj['response']['players']['player'][$i]);
+      } 
+    }
+
   }
 
   $url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0001/?key=" . $KEY . "&steamids=" . $ids;
   $xml = file_get_contents($url);
   $obj = json_decode($xml, true);
-  var_dump($obj);
+  //var_dump($obj);
 
   $sub = $obj['response']['players']['player'];
   //echo count($sub);
 
-  // container div here?
+  // since there is the possibility there multiple calls to GetPlayerSummaries this will result in multiple json objects
+  // i need to combine them here into a single array which the following code will use
+  // just extract the ['player'][x] from all arrays and build a master
+
+  echo "<div class=\"container\">";
 
   // do a first pass over the array and print those who are ingame
   for($i = 0; $i < count($sub); $i+=1)  {
@@ -64,8 +175,10 @@
     }
   }
 
+  echo "</div>";
+
   function display_dsr($player) {
-    echo "<div width=\"300px\">";
+    echo "<div class=\"dsr\">";
     echo "<img src=\"" . $player['avatarfull'] . "\" alt=\"" . $player['personaname'] . "'s Avatar\" width=\"100\" height=\"100\">";
     echo "<a href=\"" . $player['profileurl'] . "\">" . $player['personaname'] . "</a>";
     echo "<h1>" . $player['gameextrainfo'] . "</h1>";
@@ -74,13 +187,12 @@
   }
 
   function display($player) {
-    echo "<div width=\"300px\">";
+    echo "<div class=\"reg\">";
     echo "<img src=\"" . $player['avatarfull'] . "\" alt=\"" . $player['personaname'] . "'s Avatar\" width=\"100\" height=\"100\">";
     echo "<a href=\"" . $player['profileurl'] . "\">" . $player['personaname'] . "</a>";
     echo "<h1>" . $player['gameextrainfo'] . "</h1>";
     echo "<p> Last Online: " . date('Y-m-d h:i:s',$player['lastlogoff']) . "</p>";
     echo "</div>\n";
-
   }
 
 /* 
@@ -133,8 +245,6 @@
 
 
   }*/
-
-
 
   ?>
 
